a08945d40c06e95f9126991b292b5defc18a6251
Darpan Mahida <dmahida4227@conestogac.on.ca>
2023-10-16 14:24:13 -0400

    Added 2  Test For Its  Pleasant  Outside

--- TempConversionTest/Class1.cs
+++ TempConversionTest/Class1.cs
@@ -12,7 +12,7 @@ namespace TempConversionTest
     public class Class1
     {
         [Test] 
-        public void TestTempConverter_Returns_TooHot() 
+        public void Test1TempConverter_Returns_TooHot() 
         {
             // Arrange
             var temp = new Temp();
@@ -24,11 +24,63 @@ namespace TempConversionTest
           // Assert
             Assert.AreEqual("It's Too Hot!!", result);
 
+        }
+        [Test]
+        public void Test2TempConverter_Returns_TooHot()
+        {
+            // Arrange
+            var temp = new Temp();
+            float input = 38.0f;
 
+            // Act
+            var result = temp.TemperatureConverter(input);
 
+            // Assert
+            Assert.AreEqual("It's Too Hot!!", result);
 
+        }
+        [Test]
+        public void Test3TempConverter_Returns_TooHot()
+        {
+            // Arrange
+            var temp = new Temp();
+            float input = 39.5f;
+
+            // Act
+            var result = temp.TemperatureConverter(input);
+
+            // Assert
+            Assert.AreEqual("It's Too Hot!!", result);
 
         }
 
-}
+        [Test]
+        public void Test1TempConverter_Returns_PleasentOutside()
+        {
+            // Arrange
+            var temp = new Temp();
+            float input = 23.5f;
+
+            // Act
+            var result = temp.TemperatureConverter(input);
+
+            // Assert
+            Assert.AreEqual("It's pleasant outside", result);
+
+        }
+        [Test]
+        public void Test2TempConverter_Returns_PleasentOutside()
+        {
+            // Arrange
+            var temp = new Temp();
+            float input = 29.5f;
+
+            // Act
+            var result = temp.TemperatureConverter(input);
+
+            // Assert
+            Assert.AreEqual("It's pleasant outside", result);
+
+        }
+    }
 }
